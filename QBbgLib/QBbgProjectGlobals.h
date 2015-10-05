6efd68a25c370a5e7c1e57a1b6be4058f6019dc1
Luca Beldi <luca.beldi@twentyfouram.com>
2015-10-05 09:46:54 +0100

    removed PRINT_RESPONSE_MESSAGE

--- QBbgLib/QBbgProjectGlobals.h
+++ QBbgLib/QBbgProjectGlobals.h
@@ -1,7 +1,7 @@
 #ifndef ProjectGlobals_h__
 #define ProjectGlobals_h__
 #ifdef _DEBUG
-#define PRINT_RESPONSE_MESSAGE //Uncomment to debug response messages from Bloomberg
+//#define PRINT_RESPONSE_MESSAGE //Uncomment to debug response messages from Bloomberg
 #endif // _DEBUG
 #ifdef QBBG_LIB_BUILD
     #define QBBG_EXPORT __declspec(dllexport)
