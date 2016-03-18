7657a98c00bce641bbde9d6ff56316bdb24e4633
Luca Beldi <luca.beldi@twentyfouram.com>
2016-03-18 18:47:30 +0000

    Added licensing information to all files

--- QBbgLib/QBbgOverride.cpp
+++ QBbgLib/QBbgOverride.cpp
@@ -1,3 +1,21 @@
+/*******************************************************************************\
+* This file is part of QBbgLib.                                                 *
+*                                                                               *
+* QBbgLib is free software : you can redistribute it and / or modify            *
+* it under the terms of the GNU Lesser General Public License as published by   *
+* the Free Software Foundation, either version 3 of the License, or             *
+* (at your option) any later version.                                           *
+*                                                                               *
+* QBbgLib is distributed in the hope that it will be useful,                    *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of                *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the                   *
+* GNU Lesser General Public License for more details.                           *
+*                                                                               *
+* You should have received a copy of the GNU Lesser General Public License      *
+* along with QBbgLib.If not, see < http://www.gnu.org/licenses/>.               *
+*                                                                               *
+\*******************************************************************************/
+
 #include "QBbgOverride.h"
 #include "private/QBbgOverride_p.h"
 #include <QDate>
--- QBbgLib/private/QBbgAbstractRequest_p.h
+++ QBbgLib/private/QBbgAbstractRequest_p.h
@@ -1,3 +1,20 @@
+/*
+This file is part of QBbgLib.
+
+QBbgLib is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+QBbgLib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with QBbgLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef QBbgAbstractRequest_p_h__
 #define QBbgAbstractRequest_p_h__
 #include "QBbgAbstractRequest.h"
